66112ebcf49afa6701c5302000c9da92a521ef8b
Hendrik Höing <hendrikhoing@gmail.com>
2021-01-13 14:51:03 +0100

    mongo connection properties

--- src/index.py
+++ src/index.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from pymongo import MongoClient
+from src.conf import properties_mongo as pm
 
 
 app = Flask(__name__)
@@ -10,7 +11,7 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 
 
-mongo_connection = 'mongodb+srv://Hendrik:m-hh-mongo@eu-m-hh.fxa4u.mongodb.net/M-HH-analysis'
+mongo_connection = pm.connection_string
 mongo_client = MongoClient(mongo_connection)
 mongo_db = mongo_client["M-HH-analysis"]
 regional_col = mongo_db["usa_analysis"]
